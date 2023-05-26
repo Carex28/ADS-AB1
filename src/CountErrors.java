a26c4f8fbd367e121937e000e6f23fe47aed2abb
mkondratov <max.k2807@gmail.com>
2023-05-26 14:32:30 +0200

    small fix

--- src/CountErrors.java
+++ src/CountErrors.java
@@ -3,11 +3,12 @@ public class CountErrors {
     public static int countErrors(int[] solution, int[][] graph1, int[][] graph2) {
         int count = 0;
         for (int i = 0; i < solution.length; i++) {
-            if(solution[i]!=-1){
+            if (solution[i] != -1) {
                 for (int j = 0; j < solution.length; j++) {
-                    if (i != j && (solution[i] > -1 || solution[j] > -1)) {
-                        if (graph1[i][j] != graph2[solution[i]][solution[j]]) {
-                            count++;
+                    if (i != j && solution[j] != -1) {                              //check
+                            if (graph1[i][j] != graph2[solution[i]][solution[j]]) {
+                                count++;
+                            }
                         }
                         // Vergleiche ein Feld im Graph1 mit dem dazugehörigen
                         // Feld in Graph2 (dazugehörig heißt, dass solution diese
@@ -15,12 +16,8 @@ public class CountErrors {
                         // Wenn es nicht übereinstimmt, zähle count hoch
 
                     }
-
-                }
             }
         }
         return count / 2;
-        // /2 zwei, weil der Fehler doppelt vorkommt
-        // (z.B. einmal bei 1 3 und bei 3 1)
     }
 }
