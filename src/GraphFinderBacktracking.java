8a984381acb3c2cc773a74117fbd27ed9e0debe7
noah <noah.eberhardt@icloud.com>
2023-05-26 14:07:51 +0200

    noah

---
+++ src/GraphFinderBacktracking.java
@@ -0,0 +1,14 @@
+public class GraphFinderBacktracking {
+
+
+    public static int countErrors(int[] solution, int[][] graph1, int[][] graph2) {
+        // FIXME: copy code from last exercise here
+    }
+
+    
+    public static int[] findBacktracking(int[][] graph1, int[][] graph2) {
+        // FIXME
+    }
+
+}
+
