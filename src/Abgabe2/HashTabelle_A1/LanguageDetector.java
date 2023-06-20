e6ddc6c293dd9d8ea2d9f3757b1c42b2aa56bd38
mkondratov <max.k2807@gmail.com>
2023-06-20 13:48:11 +0200

    Aufgabe 1

--- src/Abgabe2/HashTabelle_A1/LanguageDetector.java
+++ src/Abgabe2/HashTabelle_A1/LanguageDetector.java
@@ -18,30 +18,79 @@ public class LanguageDetector {
 
 		Entry[] table;
 		int basis;
+		int N;
+		int count;
 
 		public HashMap(int N, int basis) {
 			table = (Entry[]) Array.newInstance(Entry[].class.getComponentType(), N);
 			this.basis = basis;
+			this.N = N;
+			this.count = 0;
 		}
 
 		public double fillRatio() {
-			// FIXME: implement
-			return -1;
+			return (double) count / (double) N;
 		}
 
 		public int hashCode(String s) {
-			// FIXME: implement
-			return -1;
+			int result = 0;
+			for (int i = 0; i < N; i++) {
+				result = sondierung(i, s, result);
+				if (table[result] == null) { // Wenn platz noch nicht besetzt, return result
+					return result;
+				}
+			}
+			return -1; // Wenn kein Platz frei return -1
+		}
+
+		private int sondierung(int i, String s, int result) {
+			if (i == 0) { // g(0) = h(s)
+				return hash(s);
+			} else { // g(m) =( g(m) + 2 * m + 1) % N
+				return (result + 2 * (i - 1) + 1) % N;
+			}
+		}
+
+		private int hash(String s) {
+			char[] cs = s.toCharArray(); // String zu CharArray
+			double result = 0;
+			for (int i = 0; i < cs.length; i++) { // Funktion in Aufgabenstellung angewandt
+				result += (cs[cs.length - i - 1] * Math.pow(basis, i)) % N;
+			}
+			return (int) result;
 		}
 
 		public T get(String key) {
-			// FIXME: implement
-			return null;
+			Entry entry;
+			int hashCode = 0;
+			for (int i = 0; i < N; i++) {
+				hashCode = sondierung(i, key, hashCode);
+				entry = table[hashCode];
+				if (entry == null) { // Wenn key nicht existent, return null
+					return null;
+				}
+				if (entry.key == key) { // Wenn key übereinstimmt, return value
+					return entry.value;
+				} // sonst continue loop mit neuem hashCode
+			}
+			return null; // Key nicht gefunden, return null
 		}
 
 		public boolean add(String key, T value) {
-			// FIXME: implement
-			return true;
+			int hashCode = 0;
+			for (int i = 0; i < N; i++) {
+				hashCode = sondierung(i, key, hashCode);
+				if (hashCode < 0) { // Wenn sondierung -1 -> kein Platz in HashMap, return false
+					return false;
+				}
+				if (table[hashCode] == null // Entweder Key gibt es noch nicht
+						|| table[hashCode].key == key) { // Oder Key gibt es schon (überschreibe Key)
+					count++;
+					table[hashCode] = new Entry(key, value);
+					return true;
+				}
+			}
+			return false;
 		}
 
 	}
