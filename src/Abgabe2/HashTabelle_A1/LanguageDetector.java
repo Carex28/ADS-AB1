890664fd7c35d157ef646e402bb04ed4fa5be281
noah <noah.eberhardt@icloud.com>
2023-06-22 13:21:27 +0200

    nix Wichtiges

--- src/Abgabe2/HashTabelle_A1/LanguageDetector.java
+++ src/Abgabe2/HashTabelle_A1/LanguageDetector.java
@@ -38,25 +38,25 @@ public class LanguageDetector {
 			int result = 0;
 			for (int i = 0; i < capacity; i++) {
 				result = sondierung(i, s, result);
-				if (table[result] == null) { // Wenn platz noch nicht besetzt, return result
+				if (table[result] == null) { 						// Wenn platz noch nicht besetzt, return result
 					return result;
 				}
 			}
-			return -1; // Wenn kein Platz frei return -1
+			return -1; 												// Wenn kein Platz frei return -1
 		}
 
-		private int sondierung(int i, String s, int result) {
-			if (i == 0) { // g(0) = h(s)
+		private int sondierung(int i, String s, int result) {		//Horner-Schema
+			if (i == 0) { 											// g(0) = h(s)
 				return hash(s);
-			} else { // g(m) =( g(m) + 2 * m + 1) % N
+			} else { 												// g(m) =( g(m) + 2 * m + 1) % N
 				return (result + 2 * (i - 1) + 1) % capacity;
 			}
 		}
 
 		private int hash(String s) {
-			char[] cs = s.toCharArray(); // String zu CharArray
+			char[] cs = s.toCharArray(); 							// String zu CharArray
 			double result = 0;
-			for (int i = 0; i < cs.length; i++) { // Funktion in Aufgabenstellung angewandt
+			for (int i = 0; i < cs.length; i++) { 					// Funktion in Aufgabenstellung angewandt
 				result = ((result * basis + cs[i]) % capacity);
 			}
 			return (int) result;
@@ -64,32 +64,34 @@ public class LanguageDetector {
 
 		public T get(String key) {
 			Entry entry;
-			int hashCode = 0;
+			int hashCode = 0;										//position
 			for (int i = 0; i < capacity; i++) {
-				hashCode = sondierung(i, key, hashCode);
+				hashCode = sondierung(i, key, hashCode);			//position errechnen
 				entry = table[hashCode];
-				if (entry == null) { // Wenn key nicht existent, return null
+				if (entry == null) { 								// Wenn key nicht existent, return null
 					return null;
 				}
-				if (entry.key == key || entry.key.equals(key)) { // Wenn key übereinstimmt, return value
+				if (entry.key == key || entry.key.equals(key)) { 	// Wenn key übereinstimmt, return value
 					return entry.value;
-				} // sonst continue loop mit neuem hashCode
+				} 													// sonst continue loop mit neuem hashCode
 			}
-			return null; // Key nicht gefunden, return null
+			return null;	 										// Key nicht gefunden, return null
 		}
 
 		public boolean add(String key, T value) {
-			int hashCode = 0;
+			int hashCode = 0;										//position
 			for (int i = 0; i < capacity; i++) {
-				hashCode = sondierung(i, key, hashCode);
-				if (hashCode < 0) { // Wenn sondierung -1 -> kein Platz in HashMap, return false
+				hashCode = sondierung(i, key, hashCode);			//position errechnen
+				if (hashCode < 0) { 								// Wenn sondierung -1 -> kein Platz in HashMap, return false
 					return false;
 				}
-				if (table[hashCode] == null // Entweder Key gibt es noch nicht
-						|| table[hashCode].key == key || table[hashCode].key.equals(key)) { // Oder Key gibt es schon
-																							// (überschreibe Key)
-					count++;
-					table[hashCode] = new Entry(key, value);
+				if (table[hashCode] == null 						// Entweder Key gibt es noch nicht
+						|| table[hashCode].key == key				// Oder Key gibt es schon
+						|| table[hashCode].key.equals(key)) {
+					if(table[hashCode] == null ){
+						count++;									// größe der hashmap
+					}
+					table[hashCode] = new Entry(key, value);		// (überschreibe Key)
 					return true;
 				}
 			}
